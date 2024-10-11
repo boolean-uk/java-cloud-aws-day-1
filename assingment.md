efec4ab8708702e91f5acf0594c3e7f77fffc669
Valen98 <leo.wahlandtcs@gmail.com>
2024-10-11 14:32:59 +0200

    Leo Wahlandt

---
+++ assingment.md
@@ -0,0 +1,5 @@
+Finished with putting my application on AWS.
+Both backend and frontend. 
+
+THE URL:
+http://aws-leo-wahlandt-day-1.s3-website-eu-west-1.amazonaws.com/
--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,5 +1,5 @@
 import "react";
 
-const url = import.meta.env.API_ENDPOINT_URL || "http://localhost:5168";
+const url = import.meta.env.API_ENDPOINT_URL || "http://aws-java-day-1-leo-wahlandt-api-env.eba-qtyxq3ym.eu-west-1.elasticbeanstalk.com/";
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
