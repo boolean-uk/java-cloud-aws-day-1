76fa9d294da14668069da1176cccf4aa68b942d8
JosteinRuen <99617155+JosteinRuen@users.noreply.github.com>
2024-10-11 15:21:25 +0200

    core + ext

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,5 +1,5 @@
 import "react";
 
-const url = import.meta.env.API_ENDPOINT_URL || "http://localhost:5168";
+const url = import.meta.env.API_ENDPOINT_URL || "http://SpringtodoJosteinRuen-env.eba-nykmunbc.eu-west-1.elasticbeanstalk.com";
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
